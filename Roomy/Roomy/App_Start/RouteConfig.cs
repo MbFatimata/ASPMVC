4c9e714b303d493d81f61b8f2bd6abf92db805fb
fati <fatimata.mbaye@hotmail.com>
2018-07-13 16:44:07 +0200

    ajout nuget bootstap ajout menu

--- Roomy/Roomy/App_Start/RouteConfig.cs
+++ Roomy/Roomy/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Roomy
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AboutRoute",
+                url: "a-propos",
+                defaults : new {controller ="Home", action ="About"}
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
