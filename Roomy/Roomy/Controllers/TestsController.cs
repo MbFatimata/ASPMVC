39c2503b83b9ee2d619c6574964f51b570eaf324
fati <fatimata.mbaye@hotmail.com>
2018-07-13 14:23:58 +0200

    ajout controller de tests

---
+++ Roomy/Roomy/Controllers/TestsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Tests
+        public ActionResult Index()
+        {
+            //EmptyResult retourn un 200 vide;
+            //FileResult retourn un 200 avec un download;
+            //HttpStatusCodeResult 
+            //JsonResult 200 avec json return Json("");
+            //RedirectResult return Redirect();
+            //ViewResult 
+            return View();
+        }
+    }
+}
