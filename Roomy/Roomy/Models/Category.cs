a947bc19417655c897936a9d59db2dffa7df391c
fati <fatimata.mbaye@hotmail.com>
2018-07-19 11:49:42 +0200

    ajout font awesome creation Classe métier Category

---
+++ Roomy/Roomy/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class Category:BaseModel
+    {
+        [Required(ErrorMessage = "Nom obligatoire")]
+        [StringLength(15, ErrorMessage = "Trop long")]
+        [Display(Name = "Libellé")]
+        public string Name { get; set; }
+    }
+}
